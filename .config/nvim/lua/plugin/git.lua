b967b4b1b68db2d13521895ef93169ed571fa584
gactocat <gactocat@gmail.com>
2023-04-04 09:56:35 +0900

    Change keybinds for git

--- .config/nvim/lua/plugin/git.lua
+++ .config/nvim/lua/plugin/git.lua
@@ -3,16 +3,16 @@ if not git_status then return end
 
 git.setup {
   keymaps = {
-    blame = '<Leader>gb',
+    blame = '<Leader>vb',
     quit_blame = 'q',
-    blame_commit = '<CR>',
-    browse = '<Leader>go',
-    open_pull_request = '<Leader>gp',
-    create_pull_request = '<Leader>gn',
-    diff = '<Leader>gd',
-    diff_close = '<Leader>gD',
-    revert = '<Leader>gr',
-    revert_file = '<Leader>gR',
+    blame_commit = '<cr>',
+    browse = '<Leader>vo',
+    open_pull_request = '<Leader>vp',
+    create_pull_request = '<Leader>vn',
+    diff = '<Leader>vd',
+    diff_close = '<Leader>vD',
+    revert = '<Leader>vr',
+    revert_file = '<Leader>vR',
   },
   target_branch = 'master',
 }
