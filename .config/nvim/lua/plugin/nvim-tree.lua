0a86466f404f8b11da3d772107cdbafad28eabf6
gactocat <gactocat@gmail.com>
2023-04-30 22:20:27 +0900

    Update `nvim-tree` settings

--- .config/nvim/lua/plugin/nvim-tree.lua
+++ .config/nvim/lua/plugin/nvim-tree.lua
@@ -1,9 +1,7 @@
 local nvim_tree_status, nvim_tree = pcall(require, 'nvim-tree')
 if not nvim_tree_status then return end
 
-nvim_tree.setup {
---  open_on_setup = true,
-}
+nvim_tree.setup {}
 
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
