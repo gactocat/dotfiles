012ba3ff43438192c46904157174fb5295b76502
gactocat <gactocat@gmail.com>
2023-04-30 23:23:09 +0900

    Setup bufferline

--- .config/nvim/lua/plugin/bufferline.lua
+++ .config/nvim/lua/plugin/bufferline.lua
@@ -1,7 +1,23 @@
 local bufferline_status, bufferline = pcall(require, 'bufferline')
 if not bufferline_status then return end
 
-bufferline.setup {}
+bufferline.setup {
+  options = {
+    diagnostics = 'nvim_lsp',
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end,
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        highlight = "Directory",
+        separator = true -- use a "true" to enable the default, or set your own character
+      }
+    }
+  }
+}
 
 local keymap = vim.keymap.set
 
