83deae377f050be09d8f1de9dd1743020848102b
gactocat <gactocat@gmail.com>
2023-05-16 18:12:19 +0900

    Add noice and alpha to nvim

--- .config/nvim/lua/plugin/lsplines.lua
+++ .config/nvim/lua/plugin/lsplines.lua
@@ -2,3 +2,5 @@ local lsplines_status, lsplines = pcall(require, 'lsp_lines')
 if not lsplines_status then return end
 
 lsplines.setup {}
+
+vim.keymap.set('', '<Leader>l', lsplines.toggle, { desc = 'Toggle lsp_lines' })
---
+++ .config/nvim/lua/plugin/noice.lua
@@ -0,0 +1,20 @@
+local noice_status, noice = pcall(require, 'noice')
+if not noice_status then return end
+
+noice.setup {
+  lsp = {
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+      ['cmp.entry.get_documentation'] = true,
+    },
+  },
+  -- options
+  presets = {
+    bottom_search = false, -- use a classic bottom cmdline for search
+    command_palette = false, -- position the cmdline and popupmenu together
+    long_message_to_split = false, -- long messages will be sent to a split
+    inc_rnname = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+}
