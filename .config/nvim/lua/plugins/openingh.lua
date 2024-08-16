8065808abcf0fa4598fb338bbaf8b93c3f339249
gactocat <gactocat@gmail.com>
2024-08-17 01:09:23 +0900

    add openingh to neovim

---
+++ .config/nvim/lua/plugins/openingh.lua
@@ -0,0 +1,8 @@
+return {
+  'almo7aya/openingh.nvim',
+  keys = {
+    { '<leader>gr', '<cmd>OpenInGHRepo<cr>', desc = 'Open repository in GitHub' },
+    { '<leader>go', '<cmd>OpenInGHFile<cr>', desc = 'Open file in GitHub' },
+    { '<leader>go', mode = 'v', '<cmd>OpenInGHFileLines<cr>', desc = 'Open file lines in GitHub' },
+  },
+}
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,33 +1,32 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 
 return {
-	-- color_scheme = 'Gruvbox Dark',
-	-- color_scheme = "nightfox",
-	color_scheme = "tokyonight",
-	colors = {
-		background = "#202020",
-	},
-	-- enable_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	hide_mouse_cursor_when_typing = true,
-	default_cursor_style = "SteadyBlock",
-	font = wezterm.font_with_fallback({
-		"RobotoMono Nerd Font",
-		-- 'Hack Nerd Font Mono',
-		-- 'SauceCodePro Nerd Font Mono',
-		-- 'mplus Nerd Font Mono',
-	}),
-	font_size = 13,
-	keys = {
-		{ key = "/", mods = "CTRL", action = wezterm.action.SendString("\x1f") },
-	},
-	window_padding = {
-		left = 10,
-		right = 10,
-		top = 10,
-		bottom = 10,
-	},
-	window_decorations = "RESIZE",
-	window_background_opacity = 0.9,
-	macos_window_background_blur = 16,
+  color_scheme = 'tokyonight',
+  colors = {
+    background = '#181818',
+  },
+
+  hide_tab_bar_if_only_one_tab = true,
+  hide_mouse_cursor_when_typing = true,
+
+  font = wezterm.font_with_fallback {
+    'RobotoMono Nerd Font',
+    -- 'Hack Nerd Font Mono',
+    -- 'SauceCodePro Nerd Font Mono',
+  },
+  font_size = 13,
+
+  keys = {
+    { key = '/', mods = 'CTRL', action = wezterm.action.SendString '\x1f' },
+  },
+
+  window_decorations = 'RESIZE',
+  window_padding = {
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
+  },
+  window_background_opacity = 0.8,
+  macos_window_background_blur = 10,
 }
